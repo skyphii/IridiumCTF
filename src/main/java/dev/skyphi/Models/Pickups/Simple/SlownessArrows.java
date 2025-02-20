161ec31791b73fa75f250033b109a891f6073365
Sky <45436884+skyphii@users.noreply.github.com>
2025-02-20 17:58:05 -0500

    Replace deprecated spigot call

--- src/main/java/dev/skyphi/Models/Pickups/Simple/SlownessArrows.java
+++ src/main/java/dev/skyphi/Models/Pickups/Simple/SlownessArrows.java
@@ -4,7 +4,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
-import org.bukkit.potion.PotionData;
 import org.bukkit.potion.PotionType;
 
 import dev.skyphi.Models.Pickups.Pickup;
@@ -16,7 +15,7 @@ public class SlownessArrows extends Pickup {
         description = ChatColor.GOLD+"Pew pew. Look how slow they are!";
         itemStack = new ItemStack(Material.TIPPED_ARROW, 4);
         PotionMeta meta = (PotionMeta)itemStack.getItemMeta();
-        meta.setBasePotionData(new PotionData(PotionType.SLOWNESS));
+        meta.setBasePotionType(PotionType.SLOWNESS);
         itemStack.setItemMeta(meta);
         init();
     }
